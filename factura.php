c37fa677ae90c608e2805994bf0c8b3ec1a59e66
Marco González <44656686+TheUltimateSpook@users.noreply.github.com>
2019-02-20 16:48:48 -0600

    Interfaz de factura en progreso
    Apenas se le agrego el bootstrap

---
+++ factura.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="../lib/jquery/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="../lib/popper/popper.min.js"></script>
+    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../lib/bootstrap/css/estilo.css">
+    <script type="text/javascript" src="../lib/bootstrap/js/bootstrap.js"></script>
+    <title>Factura</title>
+  </head>
+
+<body>
+<br>
+
+        <form class="form-inline my-2 my-lg-0" id="inicio">
+  		  </br>
+
+
+      		<ul class="nav">
+            <a &nbsp class="navbar-brand" href="#">AF</a>
+      		<li><a href="index.php"><span class="icon-home"></span>  INICIO </a></li>
+      		<li><a href=""><span class="icon-price-tags"></span> GASTOS </a>
+      			<ul>
+      		        <li><a href="modulos/mensual.php"><span class="icon-man-woman"></span> Mensual </a></li>
+
+      		        <li><a href="modulos/anual.php"><span class="icon-mobile"></span>Anual</a></li>
+      		        <li><a href="modulos/movimientos.php"><span class="icon-headphones"></span> Movimientos </a></li>
+
+      			</ul>
+      		</li>
+      	<li><a href="modulos/factura.php"><span class="icon-coin-dollar"></span> ESTADISTICAS </a></li>
+      	<li><a href="modulos/contacto.php"><span class="icon-mail4"></span> FACTURA </a></li>
+      	<li><a href="modulos/carrito.php"><span class="icon-cart"></span>  GENERAR FACTURA </a></li>
+          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+          <li><a href="modulos/carrito.php"><span class="icon-cart"></span>  PERFIL </a></li>
+      	</ul>
+
+
+
+
+
+  </form>
+  <br>
+  <div class="card text-center">
+    <div class="card-header">
+    </div>
+    <div class="card-body">
+      <h3 align = left class="card-title">No.Factura#</h3>
+      <h3 id="Fecha">Fecha:</h3>
+      <a href="#" class="btn btn-primary">Guardar Factura</a>
+    </div>
+  </div>
+  </body>
+</html>
