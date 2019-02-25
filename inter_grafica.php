ea8d3c715cf7aa06d2a747d79eaf35d283203650
Luis Fernando Rodriguez Gutierrez <44719831+LuisFernandoRG@users.noreply.github.com>
2019-02-24 18:33:35 -0600

    Add files via upload
    sin bootstrap aun.

---
+++ inter_grafica.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<title>Insertar</title>
+</head>
+<?php 
+print "<img src=\"logouwu.png\">" 
+?>
+<body>
+<h1>Graficas de los registros</h1>
+<br>
+<form method="post" action="alta_usuario.php">
+Estadisiticas:
+<select name="sltcate">
+<option value="0">uwu</option>
+<option value="1">uwu</option>
+<option value="2">uwu</option>
+<option value="3">uwu</option>
+</select>
+<br>
+Gastos:
+<select name="sltcate">
+<option value="0">uwu</option>
+<option value="1">uwu</option>
+<option value="2">uwu</option>
+<option value="3">uwu</option>
+</select>
+<br>
+
+Generar facutura:
+<select name="sltcate">
+<option value="0">uwu</option>
+<option value="1">uwu</option>
+<option value="2">uwu</option>
+<option value="3">uwu</option>
+</select>
+<br>
+Perfil:
+<select name="sltcate">
+<option value="0">uwu</option>
+<option value="1">uwu</option>
+<option value="2">uwu</option>
+<option value="3">uwu</option>
+</select>
+<br>
+</form>
+     </div>
+        </body>
+        
+        <body>
+        
+        <form action="">
+		<h2>Tipo de graficas</h2>
+
+
+	   <div class="row">
+		   <div class="col-12 col-sm-6 col-md-6 col-lg-6">
+
+           <input type="radio" class="grafica" name="grafica"grafica" value="Barras">Barras</br>
+           <input type="radio" class="grafica" name="grafica"grafica" value="Lineas">Lineas</br>
+           <input type="radio" class="grafica" name="grafica"grafica" value="Pastel">Pastel</br>
+           <input type="radio" class="grafica" name="grafica"grafica" value="Histograma">Histograma</br>
+
+
+		   </div>
+
+	  </div>
+	  
+
+  </form>
+        </body>
+        <body>
+        <div align="left">
+         <h1>Consultar fecha</h1>
+         <br>
+        <form method="post" action="alta_usuario.php">
+Dia:
+<select name="sltcate">
+<option value="0">1</option>
+<option value="1">2</option>
+<option value="2">3</option>
+<option value="3">4</option>
+<option value="0">5</option>
+<option value="1">6</option>
+<option value="2">7</option>
+<option value="3">8</option>
+<option value="0">9</option>
+<option value="1">10</option>
+<option value="2">11</option>
+<option value="3">12</option>
+<option value="0">13</option>
+<option value="1">14</option>
+<option value="2">15</option>
+<option value="3">16</option>
+</select>
+<br>
+Mes:
+<select name="sltcate">
+<option value="0">Enero</option>
+<option value="1">Febrero</option>
+<option value="2">Marzo</option>
+<option value="3">Abril</option>
+</select>
+<br>
+
+a&ntildeo:
+<select name="sltcate">
+<option value="0">2018</option>
+<option value="1">2017</option>
+<option value="2">2016</option>
+<option value="3">2015</option>
+</select>
+<br>
+</form>
+        </body>
+</html>
