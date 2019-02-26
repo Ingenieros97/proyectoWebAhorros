32e30022f4915dfb1c96f85468f2ebaa72326155
Yessivr982218 <44659938+Yessivr982218@users.noreply.github.com>
2019-02-25 21:04:40 -0600

    Add files via upload
    Inicio

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="lib/jquery/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="lib/popper/popper.min.js"></script>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="lib/bootstrap/css/estilo.css">
+    <script type="text/javascript" src="lib/bootstrap/js/bootstrap.js"></script>
+    <title>Inicio</title>
+  </head>
+  <body>
+<br>
+
+        <form class="form-inline my-2 my-lg-0" id="inicio">
+  		  </br>
+
+
+      		<ul class="nav">
+            <a &nbsp class="navbar-brand" href="#">AF</a>
+      		<li><a href="index.php"><span class="icon-home"></span>  INICIO </a></li>
+      		<li><a href=""><span class="icon-price-tags"></span> GASTOS </a>
+      			<ul>
+      		        <li><a href="modulos/ropa.php"><span class="icon-man-woman"></span> Mensual </a></li>
+
+      		        <li><a href="modulos/accesorios.php"><span class="icon-mobile"></span> Anual </a></li>
+      		        <li><a href="modulos/muscia.php"><span class="icon-headphones"></span> Movimientos </a></li>
+
+      			</ul>
+      		</li>
+      	<li><a href="modulos/descuentos.php"><span class="icon-coin-dollar"></span> ESTADISTICAS </a></li>
+      	<li><a href="modulos/contacto.php"><span class="icon-mail4"></span> FACTURA </a></li>
+      	<li><a href="modulos/carrito.php"><span class="icon-cart"></span>  GENERAR FACTURA </a></li>
+          &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+          <li><a href="modulos/carrito.php"><span class="icon-cart"></span>  PERFIL </a></li>
+      	</ul>
+
+
+
+
+
+  </form>
+
+  <br>
+
+  <center><div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.jpg" class="d-block" alt="..." width="450" height="250">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.jpg" class="d-block" alt="..." width="450" height="250">
+    </div>
+    <div class="carousel-item">
+      <img src="img/3.jpg" class="d-block" alt="..." width="450" height="250">
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+
+
+    </a>
+  </div>
+
+</div></center>
+
+<div class="row">
+  <div class="col-12 col-sm-6 col-md-6 col-lg-6  border-warning mb-3">
+    <form id="informacion1">
+    INGRESA DESDE TU CELULAR EN CUALQUIER MOMENTO
+    </form>
+  </div>
+</div>
+
+
+<footer class="page-footer font-small unique-color-dark pt-4">
+
+    <!-- Footer Elements -->
+    <div class="container">
+
+      <!-- Call to action -->
+      <ul class="list-unstyled list-inline text-center py-2">
+        <li class="list-inline-item">
+          <h5 class="mb-1">Siguenos en: </h5>
+        </li>
+        <li class="list-inline-item">
+          <a href="registro.php" class="btn btn-outline-white btn-rounded">Facebook</a>
+            <a href="registro.php" class="btn btn-outline-white btn-rounded">Twitter</a>
+        </li>
+      </ul>
+      <!-- Call to action -->
+
+    </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a >Ahorro Fácil</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+
+  </body>
+</html>
